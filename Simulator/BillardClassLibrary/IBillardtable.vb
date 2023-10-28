732ee3490594627b15510fd071af355bfe72f562
Hermann Biner <148759915+HermannBiner@users.noreply.github.com>
2023-10-28 12:32:06 +0200

    Add files via upload

---
+++ Simulator/BillardClassLibrary/IBillardtable.vb
@@ -0,0 +1,16 @@
+﻿'Interface for all kind of billard tables
+'Status Checked
+
+Public Interface IBillardtable
+
+    'the billard table is drawn into the bitmap
+    WriteOnly Property MapBillardtable As Bitmap
+
+    'the parameter C defines the profile of the billard table
+    WriteOnly Property C As Decimal
+
+    Sub DrawBillardtable()
+
+    Sub Clear()
+
+End Interface
---
+++ Simulator/BillardClassLibrary/ICDiagram.vb
@@ -0,0 +1,26 @@
+﻿'Interface for the study of the influence of a parameter C
+'defining the profile of Billards or Pendulum
+'to their parameters like Hitpoins or Angles
+'describing their dynamic behaviour
+
+
+Public Interface ICDiagram
+
+    'The Parameter C steers the profile of the investigated danymic system
+    'and is set in the beginning of the iteration for this C
+    WriteOnly Property CParameter As Decimal
+
+    'depending on the type of iteration, 
+    'the parameter C is in a certain interval
+    ReadOnly Property CParameterRange As ClsInterval
+
+    'ClsValueRange contains all necessary information of a valuerange
+    'like the reflexion angle of a Billard
+    'and ValueRanges is the List of such Valueranges
+    'of the dynamic system
+    ReadOnly Property ValueParameters As List(Of ClsValueParameter)
+
+    'This is the main Function to get the next Iteration Point
+    Function GetNextPoint(ActualPoint As ClsValueTupel) As ClsValueTupel
+
+End Interface
